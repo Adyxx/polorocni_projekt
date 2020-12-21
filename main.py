b4756630978d9d3eba408289409d2d8248b9b791
strohli <libor@f13cybertech.com>
2020-12-21 13:49:04 +0100

    obrázek je viditelný a skoro funguje :)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import time
 
-
+import sprites
 from pygame.locals import (
     RLEACCEL,
     K_UP,
@@ -12,25 +12,34 @@ from pygame.locals import (
     K_ESCAPE,
     KEYDOWN,
     QUIT,
+    MOUSEBUTTONDOWN,
 )
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
+cislo = 1
 class Foto(pygame.sprite.Sprite):
     def __init__(self):
         super(Foto, self).__init__()
 
         # 800x468 rozměry všech fotek
-        self.surf = pygame.image.load("foto/opava1.jpg").convert()
+        self.surf = pygame.image.load(f"foto/opava{cislo}.jpg").convert()
 
         self.rect = self.surf.get_rect(
             center=((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2 - 90),
             )
         )
     # pohyb hráče
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
+    def update(self, pressed_keys, cislo):
+        if event.type == pygame.MOUSEBUTTONUP:
+            cislo += 1
+            if cislo == 2:
+                cislo = 1
+            self.surf = pygame.image.load(f"foto/opava{cislo}.jpg").convert()
+
+
+
+        ''' if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
 
         if pressed_keys[K_DOWN]:
@@ -42,15 +51,7 @@ class Foto(pygame.sprite.Sprite):
             self.rect.move_ip(5, 0)
     # objekt zůstane na obrazovce
         '''
-        if self.rect.left < 0:
-            self.rect.left =0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top =0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-'''
+
 
 pygame.mixer.init()
 
@@ -87,9 +88,8 @@ while running:
 
 
 
-    pressed_keys = pygame.key.get_pressed()
-
-    foto.update(pressed_keys)
+    pressed_keys = pygame.mouse.get_pressed()
+    foto.update(pressed_keys, cislo)
 
 
 
@@ -104,6 +104,6 @@ while running:
 
     pygame.display.flip()
 
-clock.tick(45)
+    clock.tick(45)
 
 pygame.mixer.quit()
