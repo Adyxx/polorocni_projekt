c9fe60f537f6acc142d3f7abef77a5e21f4c9718
strohli <libor@f13cybertech.com>
2020-12-22 16:45:41 +0100

    IT_FUCKING_WORKS!!!!!!!

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ from pygame.locals import (
 )
 standing = True
 
-score = 0
+
 rozhodnuti1 = 0
 rozhodnuti = 0
 try:
     f = open("score.txt", "r")
 except:
     f = open("score.txt", "w")
+    f.write("0")
     f.close()
     f = open("score.txt", "r")
 
-print(f.read())
-
+print()
+score = 0
+h_score = f.read()
 class Foto(pygame.sprite.Sprite):
     def __init__(self):
         super(Foto, self).__init__()
@@ -138,30 +140,50 @@ while standing:
         return
 
 
-    def is_it_right(a):
+    def is_it_right(a, f):
+        f = open("score.txt", "r")
         global score
+        global h_score
         if misto == 'opava' and val[a] == 'Opava':
-            print('yes')
             score +=1
+            if (score > int(f.read())):
+                f.close()
+                f = open("score.txt", "w")
+                f.write(str(score))
+                f.close()
+                h_score = score
         elif misto == 'ostrava' and val[a] == 'Ostrava':
-            print('yes')
             score +=1
+            if (score > int(f.read())):
+                f.close()
+                f = open("score.txt", "w")
+                f.write(str(score))
+                f.close()
+                h_score = score
         elif misto == 'paris' and val[a] == 'Paříž':
-            print('yes')
             score +=1
+            if (score > int(f.read())):
+                f.close()
+                f = open("score.txt", "w")
+                f.write(str(score))
+                f.close()
+                h_score = score
         elif misto == 'praha' and val[a] == 'Praha':
-            print('yes')
             score +=1
+            if (score > int(f.read())):
+                f.close()
+                f = open("score.txt", "w")
+                f.write(str(score))
+                f.close()
+                h_score = score
         else:
-            print('no')
             score = 0
 
         return 
-        
-    print(score)
+
 
     # zjistí index správné odpovědi a nahradí jej pokud neexistuje tlačítko se správnou odpovědí
-    print(val)
+
     if misto == 'opava':
         x = val.index('Opava')
         answer_on_button()
@@ -174,7 +196,7 @@ while standing:
     elif misto == 'ostrava':
         x = val.index('Ostrava')
         answer_on_button()
-    print(val)
+
 
 
     pygame.mixer.init()
@@ -216,27 +238,25 @@ while standing:
             # při kliknutí update foto
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if width/2 - 390 <= mouse[0] <= width/2 - 10 and height/2 + 222 <= mouse[1] <= height/2 + 292:
-                    print(val, val[0]) 
 
-                    is_it_right(0)
+
+                    is_it_right(0, f)
 
                     running = False
                 elif width/2 - 390 <= mouse[0] <= width/2 - 10 and height/2 + 147 <= mouse[1] <= height/2 + 217:
-                    print(val, val[1])
 
-                    is_it_right(1)
+
+                    is_it_right(1, f)
                     
                     running = False
                 elif width/2 + 10 <= mouse[0] <= width/2 + 390 and height/2 + 147 <= mouse[1] <= height/2 + 217:
-                    print(val, val[2])
 
-                    is_it_right(2)
+                    is_it_right(2, f)
 
                     running = False
                 elif width/2 + 10 <= mouse[0] <= width/2 + 390 and height/2 + 222 <= mouse[1] <= height/2 + 292:
-                    print(val, val[3])
 
-                    is_it_right(3)
+                    is_it_right(3, f)
 
                     running = False
                 else:
@@ -257,7 +277,7 @@ while standing:
         ###################################################
         text = pygame.font.SysFont('Arial Black', 50)
         score_text = pygame.font.SysFont('Comic Sans', 40)
-
+        h_score_text = pygame.font.SysFont('Comic Sans', 40)
         text_b1 = text.render(f'{val[0]}', True, (255, 255, 255))
         textRect = text_b1.get_rect()
         textRect.center = (width / 2 - 200, height / 2 + 252)
@@ -276,7 +296,11 @@ while standing:
 
         text_score = score_text.render(f'Score: {score}', False, (0,0,0))
         scoreRect = text_score.get_rect()
-        scoreRect.center = (width / 2 - 340, height / 2 - 280)
+        scoreRect.center = (width / 2 - 340, height / 2 - 250)
+
+        h_text_score = h_score_text.render(f'High score: {h_score}', False, (0, 0, 0))
+        h_scoreRect = h_text_score.get_rect()
+        h_scoreRect.center = (width / 2 - 308, height / 2 - 280)
         ###################################################
 
         for entity in all_sprites:
@@ -286,6 +310,7 @@ while standing:
             screen.blit(text_b3, textRect3)
             screen.blit(text_b4, textRect4)
             screen.blit(text_score, scoreRect)
+            screen.blit(h_text_score, h_scoreRect)
         pygame.display.flip()
         #kolik snímků za vteřinu, dosova by mohlo být tak 2, moc se to tam nehýbe :)
         clock.tick(45)
